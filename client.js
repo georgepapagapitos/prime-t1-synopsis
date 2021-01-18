3efd6d5a852fb29703d1ad738db3a1bd8a652b7a
George Papagapitos <george.g.papagapitos@gmail.com>
2021-01-18 11:58:28 -0600

    completed synopsis

--- client.js
+++ client.js
@@ -13,9 +13,43 @@ function addPurchase() {
     // push the new purchase into the array
     purchases.push(newPurchase);
     // empty inputs
-    $('#purchaseNameIn').val('')
-    $('purchasePriceIn').val('')
+    $('#itemNameIn').val('')
+    $('#itemPriceIn').val('')
     // calculate remainingBudget
+    calculateRemainingBudget();
+    // update DOM
+    displayPurchase();
+}
+
+function calculateRemainingBudget() {
+    // loop through purchases array
+    let totalPrices = 0;
+    for(let i = 0; i < purchases.length; i++) {
+        // for each purchase, add up total of all prices
+        totalPrices += Number(purchases[i].price);
+    }
+    console.log('totalPrices:', totalPrices);
+    // subtract totalPrices from the budget for remainingBudget
+    const remainingBudget = budget - totalPrices;
+    // display remainingBudget
+    let el = $('#remainingBudgetOut');
+    el.empty();
+    el.append(remainingBudget);
+}
+
+function displayPurchase() {
+    console.log('in displayPurchase');
+    // target output by id
+    let el = $('#purchasesOut');
+    // empty
+    el.empty();
+    // loop through purchases array
+    for( purchase of purchases) {
+        // for each purchase, create a list item
+        // append to ul
+        el.append(`<li>` + purchase.name + `: $` + purchase.price + `</li>`)
+    }
+    // for each purchase, create a list item
 }
 
 function readyNow() {
@@ -26,4 +60,6 @@ function readyNow() {
     el.append(budget);
     // handle click event
     $('#addPurchaseButton').on('click', addPurchase);
+    // init display
+    calculateRemainingBudget();
 }
